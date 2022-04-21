0ba2806dd8d4510b3ce6a4764d17feeb39f5a853
Kevin Voß <kevin.voss@micromerce.com>
2022-04-21 22:54:27 +0200

    adjust children type

--- src/context/ExplorerContext.types.ts
+++ src/context/ExplorerContext.types.ts
@@ -1,8 +1,9 @@
+import { ReactElement } from "react";
 import { File, Directory } from "../types/explorer";
 
 export default interface IExplorer {
   config: {
     [key: string]: File | Directory;
   };
-  children?: any;
+  children?: ReactElement[] | JSX.Element | string;
 }
