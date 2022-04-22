04837b2549518cd9b49fb09bfb48a72cf2d54a09
Kevin Voß <kevin.voss@micromerce.com>
2022-04-22 23:07:28 +0200

    add folder/file details

--- src/components/directory/Directory.tsx
+++ src/components/directory/Directory.tsx
@@ -1,7 +1,49 @@
+import { useState } from "react";
 import IDirectory from "./Directory.types";
+import Controller from "../controller/Controller";
+import styles from "./Directory.module.scss";
+import { MdKeyboardArrowRight, MdKeyboardArrowDown } from "react-icons/md";
+import {
+  AiFillFolder,
+  AiFillFolderOpen,
+  AiOutlineDelete,
+} from "react-icons/ai";
+import { BiMove } from "react-icons/bi";
 
 const Directory = ({ item }: IDirectory) => {
-  return <div>Directory</div>;
+  const [isDirectoryOpen, setIsDirectoryOpen] = useState(false);
+
+  const handleToggleDirectoryOpen = () => setIsDirectoryOpen((prev) => !prev);
+
+  return (
+    <div className={styles["directory-container"]}>
+      <div className={styles["directory-info-bar"]}>
+        <div
+          className={styles["directory-info"]}
+          onClick={handleToggleDirectoryOpen}
+        >
+          {isDirectoryOpen ? <MdKeyboardArrowDown /> : <MdKeyboardArrowRight />}
+          {isDirectoryOpen ? <AiFillFolderOpen /> : <AiFillFolder />}
+          <p className={styles["name"]}>{item.name}</p>
+        </div>
+        <div className={styles["directory-controls"]}>
+          <div className="move">
+            <BiMove />
+          </div>
+          <div className="delete">
+            <AiOutlineDelete />
+          </div>
+        </div>
+      </div>
+      {isDirectoryOpen && (
+        <div className={styles["directory-files"]}>
+          {Object.entries(item?.files).map(([key, value]: any) => {
+            return <Controller key={key} item={value} />;
+          })}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Directory;
--- src/components/file/File.tsx
+++ src/components/file/File.tsx
@@ -1,7 +1,45 @@
 import IFile from "./File.types";
+import styles from "./File.module.scss";
+import { GrDocumentTxt } from "react-icons/gr";
+import {
+  AiOutlineFileUnknown,
+  AiOutlineFilePdf,
+  AiOutlineDelete,
+} from "react-icons/ai";
+import { BiMove } from "react-icons/bi";
+import { MdOutlinePreview } from "react-icons/md";
 
 const File = ({ item }: IFile) => {
-  return <div>File</div>;
+  const fileTypeMap = {
+    txt: GrDocumentTxt,
+    pdf: AiOutlineFilePdf,
+    default: AiOutlineFileUnknown,
+  };
+
+  const Icon =
+    fileTypeMap[item.fileType as keyof typeof fileTypeMap] ||
+    fileTypeMap["default"];
+
+  return (
+    <div className={styles["file-container"]}>
+      <div className={styles["file-info"]}>
+        <Icon />
+        <p>{item?.name}</p>
+        <p>{item?.fileType}</p>
+      </div>
+      <div className={styles["file-controls"]}>
+        <div className="preview">
+          <MdOutlinePreview />
+        </div>
+        <div className="move">
+          <BiMove />
+        </div>
+        <div className="delete">
+          <AiOutlineDelete />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default File;
