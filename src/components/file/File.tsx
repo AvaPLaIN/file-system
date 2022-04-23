40eef15c3275e26cf261bc6f07c5503b23ed694c
Kevin Voß <kevin.voss@micromerce.com>
2022-04-23 21:47:23 +0200

    add editing filename for File

--- src/components/file/File.tsx
+++ src/components/file/File.tsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import {
   AiOutlineDelete,
+  AiOutlineEdit,
   AiOutlineFileImage,
   AiOutlineFilePdf,
   AiOutlineFileUnknown,
 } from "react-icons/ai";
-import { BiMove } from "react-icons/bi";
+import { BiCheck, BiMove } from "react-icons/bi";
 import { GrDocumentTxt } from "react-icons/gr";
-import { MdOutlinePreview } from "react-icons/md";
+import { MdOutlineClose, MdOutlinePreview } from "react-icons/md";
 import styles from "./File.module.scss";
 import IFile from "./File.types";
 
 const File = ({ item }: IFile) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [fileName, setFileName] = useState(item?.name);
+
   const fileTypeMap = {
     txt: GrDocumentTxt,
     pdf: AiOutlineFilePdf,
@@ -23,14 +28,41 @@ const File = ({ item }: IFile) => {
     fileTypeMap[item.fileType as keyof typeof fileTypeMap] ||
     fileTypeMap["default"];
 
+  const handleStartEditing = () => setIsEditing(true);
+  const handleStopEditing = () => setIsEditing(false);
+
+  const handleChangeFileName = (event: any) =>
+    setFileName(event?.target?.value);
+
+  const handleSaveFileName = () => {
+    setIsEditing(false);
+
+    // TODO save file name
+  };
+
   return (
     <div className={styles["file-container"]}>
       <div className={styles["file-info"]}>
         <FileTypeIcon />
-        <p>{item?.name}</p>
+        {isEditing ? (
+          <>
+            <input
+              type="text"
+              value={fileName}
+              onChange={handleChangeFileName}
+            />
+            <MdOutlineClose onClick={handleStopEditing} />
+            <BiCheck onClick={handleSaveFileName} />
+          </>
+        ) : (
+          <p>{item?.name}</p>
+        )}
         <p>.{item?.fileType}</p>
       </div>
       <div className={styles["file-controls"]}>
+        <div className="edit" onClick={handleStartEditing}>
+          <AiOutlineEdit />
+        </div>
         <div className="preview">
           <MdOutlinePreview />
         </div>
